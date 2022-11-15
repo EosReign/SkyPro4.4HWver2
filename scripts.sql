89afc22f4430089b496391c8aa8244a56c5f465b
EosReign <vsevolod_sol@mail.ru>
2022-11-15 19:51:19 +0300

    meh

--- scripts.sql
+++ scripts.sql
@@ -16,3 +16,11 @@ where student_age < student_id;
 select *
 from table_students
 order by student_age;
+
+select s.* from table_faculties as f, student as s
+where s.faculty_id = f.id
+  and f.faculty_name = 'griffindor';
+
+select f.* from table_faculties as f, student as s
+where s.faculty_id = f.id
+  and s.student_name = 'Harry Potter';
